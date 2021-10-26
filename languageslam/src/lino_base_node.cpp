94ccdebfdae984175201ad4fe8a09bc5c5cd67a2
Tristan9497 <tristan@5schwoerer.de>
2021-10-26 15:48:28 +0200

    Trying to get name from parameter server

--- languageslam/src/lino_base_node.cpp
+++ languageslam/src/lino_base_node.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv )
     ros::init(argc, argv, "lino_base_node");
     ros::NodeHandle nh("~");
     std::string param;
-    nh.getParam("robot_name_test", param);
+    nh.getParam("robot_name", param);
     ROS_WARN("Got parameter : %s", param.c_str());
     LinoBase lino;
     ros::spin();
