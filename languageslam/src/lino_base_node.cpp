cc05202972fd38898fa5a1e3b68216b8c6bb7d23
Tristan9497 <tristan@5schwoerer.de>
2021-10-26 15:37:32 +0200

    Trying sending arguments

--- languageslam/src/lino_base_node.cpp
+++ languageslam/src/lino_base_node.cpp
@@ -4,6 +4,10 @@
 int main(int argc, char** argv )
 {
     ros::init(argc, argv, "lino_base_node");
+    ros::NodeHandle nh("~");
+    std::string param;
+    nh.getParam("robot_name_test", param);
+    ROS_INFO("Got parameter : %s", param.c_str());
     LinoBase lino;
     ros::spin();
     return 0;
