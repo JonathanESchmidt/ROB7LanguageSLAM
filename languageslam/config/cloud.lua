0329c00069fe411d4b67ba4b30edc2af2f3baf11
Tristan9497 <tristan@5schwoerer.de>
2021-11-01 16:40:06 +0100

    Add pose graph config to server

--- languageslam/config/cloud.lua
+++ languageslam/config/cloud.lua
@@ -3,5 +3,11 @@ include "map_builder_server.lua"
 MAP_BUILDER_SERVER.map_builder.use_trajectory_builder_2d = true
 MAP_BUILDER_SERVER.server_address = "0.0.0.0:50100"
 MAP_BUILDER_SERVER.uplink_server_address = ""
+POSE_GRAPH.constraint_builder.min_score = 0.65
+POSE_GRAPH.optimization_problem.huber_scale = 3
 
+POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 1e3
+POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 1e2
+POSE_GRAPH.optimization_problem.odometry_translation_weight = 1e3
+POSE_GRAPH.optimization_problem.odometry_rotation_weight = 1e2
 return MAP_BUILDER_SERVER
