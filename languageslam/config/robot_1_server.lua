5ab7651f3cf6db84e672e41edfd07fd8f2dcfbef
Tristan9497 <tristan@5schwoerer.de>
2021-11-01 23:10:39 +0100

    changes to run this on the robot itself

--- languageslam/config/robot_1_server.lua
+++ languageslam/config/robot_1_server.lua
@@ -1,8 +1,8 @@
 include "map_builder_server.lua"
 
 MAP_BUILDER_SERVER.map_builder.use_trajectory_builder_2d = true
-MAP_BUILDER_SERVER.map_builder.pose_graph.optimize_every_n_nodes = 99999999999
+MAP_BUILDER_SERVER.map_builder.pose_graph.optimize_every_n_nodes = 0
 MAP_BUILDER_SERVER.server_address = "0.0.0.0:50051"
-MAP_BUILDER_SERVER.uplink_server_address = "0.0.0.0:50100"
+MAP_BUILDER_SERVER.uplink_server_address = "192.168.1.38:50100"
 
 return MAP_BUILDER_SERVER
