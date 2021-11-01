2242d0d9636ff78c7a07da4ce9a80ea860d502a6
Tristan9497 <tristan@5schwoerer.de>
2021-11-01 23:37:16 +0100

    Specify cloud ip adress exactly

--- languageslam/config/cloud.lua
+++ languageslam/config/cloud.lua
@@ -1,8 +1,10 @@
 include "map_builder_server.lua"
 
 MAP_BUILDER_SERVER.map_builder.use_trajectory_builder_2d = true
-MAP_BUILDER_SERVER.server_address = "0.0.0.0:50100"
+MAP_BUILDER_SERVER.server_address = "192.168.1.38:50100"
 MAP_BUILDER_SERVER.uplink_server_address = ""
+
+
 POSE_GRAPH.constraint_builder.min_score = 0.65
 POSE_GRAPH.optimization_problem.huber_scale = 3
 POSE_GRAPH.optimize_every_n_nodes = 15
@@ -10,4 +12,5 @@ POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 1e3
 POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 1e2
 POSE_GRAPH.optimization_problem.odometry_translation_weight = 1e3
 POSE_GRAPH.optimization_problem.odometry_rotation_weight = 1e2
+
 return MAP_BUILDER_SERVER
