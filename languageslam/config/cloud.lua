31feeeeaff89ca8b504c351ab7d823944ab6d20f
Tristan Schwoerer <tristan@5schwoerer.de>
2021-11-03 12:17:45 +0100

    ip adress forwarding in launch and config files

--- languageslam/config/cloud.lua
+++ languageslam/config/cloud.lua
@@ -1,7 +1,7 @@
 include "map_builder_server.lua"
 
 MAP_BUILDER_SERVER.map_builder.use_trajectory_builder_2d = true
-MAP_BUILDER_SERVER.server_address = "192.168.0.104:50100"
+MAP_BUILDER_SERVER.server_address = "192.168.0.110:50100"
 MAP_BUILDER_SERVER.uplink_server_address = ""
 
 
--- languageslam/config/robot_1_server.lua
+++ languageslam/config/robot_1_server.lua
@@ -1,8 +1,8 @@
 include "map_builder_server.lua"
-
+robot_adress=os.getenv("robot_ip")
 MAP_BUILDER_SERVER.map_builder.use_trajectory_builder_2d = true
 MAP_BUILDER_SERVER.map_builder.pose_graph.optimize_every_n_nodes = 9999999999999
-MAP_BUILDER_SERVER.server_address = "192.168.1.43:50051"
-MAP_BUILDER_SERVER.uplink_server_address = "192.168.1.38:50100"
+MAP_BUILDER_SERVER.server_address = robot_adess .. ":50051"
+MAP_BUILDER_SERVER.uplink_server_address = "192.168.1.110:50100"
 
 return MAP_BUILDER_SERVER
