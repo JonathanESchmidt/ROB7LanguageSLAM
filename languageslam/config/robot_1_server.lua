24894a5777c2b8a523d37195d45cc92c380055cb
Tristan Schwoerer <tristan@5schwoerer.de>
2021-11-03 12:39:19 +0100

    typo in ip

--- languageslam/config/robot_1_server.lua
+++ languageslam/config/robot_1_server.lua
@@ -2,6 +2,6 @@ include "map_builder_server.lua"
 MAP_BUILDER_SERVER.map_builder.use_trajectory_builder_2d = true
 MAP_BUILDER_SERVER.map_builder.pose_graph.optimize_every_n_nodes = 9999999999999
 MAP_BUILDER_SERVER.server_address = os.getenv("robot_ip")
-MAP_BUILDER_SERVER.uplink_server_address = "192.168.1.110:50100"
+MAP_BUILDER_SERVER.uplink_server_address = "192.168.0.110:50100"
 
 return MAP_BUILDER_SERVER
