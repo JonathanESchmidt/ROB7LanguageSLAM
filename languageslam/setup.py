d286884d1899fc846ed852d875bfd81963546d26
Tristan Schwörer <65277088+Tristan9497@users.noreply.github.com>
2021-11-11 13:54:32 +0100

    Update setup.py

--- languageslam/setup.py
+++ languageslam/setup.py
@@ -3,5 +3,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['languageslam'],
     package_dir={'': 'src'}
+    requires=['std_msgs', 'rospy', 'nav_msgs']
 )
 setup(**d)
