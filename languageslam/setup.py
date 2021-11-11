67711fe3c4f6172738c9273e8780a84fd23d3130
Tristan Schwörer <65277088+Tristan9497@users.noreply.github.com>
2021-11-11 13:25:10 +0100

    Update setup.py

--- languageslam/setup.py
+++ languageslam/setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['languageslam'],
-    scripts=['bin/map_converter'],
+    scripts=['src/map_converter'],
     package_dir={'': 'src'}
 )
 
