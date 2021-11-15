c9bb5c4a6bc693e19e23404608a1602d422308e5
Tristan Schwörer <65277088+Tristan9497@users.noreply.github.com>
2021-11-15 14:01:47 +0100

    Update SpeechWidget.py

--- rqt_mypkg/rqt_mypkg/src/rqt_mypkg/SpeechWidget.py
+++ rqt_mypkg/rqt_mypkg/src/rqt_mypkg/SpeechWidget.py
@@ -44,7 +44,7 @@ class App(QWidget):
         layout.addWidget(self.button, 2, 0)
         layout.addWidget(self.label, 3, 0)
         layout.addWidget(self.label2, 4, 0)
-        rospy.init_node('SpeechWidget')
+        rospy.init_node('SpeechCommand')
         #self.client7 = actionlib.SimpleActionClient('/robot7/move_base',MoveBaseAction)#for now commented out
         self.client8 = actionlib.SimpleActionClient('/robot8/move_base',MoveBaseAction)
         #wait for action client server
