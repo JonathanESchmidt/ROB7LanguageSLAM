d3d98f37350456f6529c5935a6398758b76ede04
Tristan Schwörer <65277088+Tristan9497@users.noreply.github.com>
2021-11-15 13:47:48 +0100

    Update SpeechWidget.py

--- rqt_mypkg/rqt_mypkg/src/rqt_mypkg/SpeechWidget.py
+++ rqt_mypkg/rqt_mypkg/src/rqt_mypkg/SpeechWidget.py
@@ -77,7 +77,7 @@ class App(QWidget):
             self.client7.send_goal(goal)
         elif robotno==8:
             self.client8.wait_for_server()
-           print("waiting for server")
+            print("waiting for server")
             self.client8.send_goal(goal)
 
         '''wait = client.wait_for_result()
