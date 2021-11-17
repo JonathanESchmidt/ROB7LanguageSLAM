9a6021c1d15b8e4ec8a26342228bf456dbf7c5b8
Tristan9497 <tristan@5schwoerer.de>
2021-11-17 17:27:17 +0100

    workspace cleanup

--- languageslam/setup.py
+++ languageslam/setup.py
@@ -2,7 +2,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['languageslam'],
-    package_dir={'': 'src'},
-    requires=['std_msgs', 'rospy', 'nav_msgs']
+    package_dir={'': 'src'}
 )
 setup(**d)
