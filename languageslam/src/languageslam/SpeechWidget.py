20a90c77d07ddb180fb1c486cd163842cfa6e594
JonathanESchmidt <33545029+JonathanESchmidt@users.noreply.github.com>
2021-12-01 10:50:41 +0100

    Split speech widget into two files

--- languageslam/src/languageslam/SpeechWidget.py
+++ languageslam/src/languageslam/SpeechWidget.py
@@ -12,9 +12,9 @@ from nltk.stem.porter import PorterStemmer
 import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from geometry_msgs.msg import Quaternion
 from tf.transformations import quaternion_from_euler
 
+from .robotCommands import robot_command as rc
 from languageslam.srv import toggleexploration
 
 class App(QWidget):
@@ -23,6 +23,10 @@ class App(QWidget):
         # Setup of the QT widget
         super(App, self).__init__()
         self.setWindowTitle('Voice Control')
+
+        # TODO add check for online robots robots
+        self.robot7 = rc(7)
+        self.robot8 = rc(8)
         
         self.check_robot7 = QCheckBox("Robot 7", self)
         self.check_robot8 = QCheckBox("Robot 8", self)
@@ -46,167 +50,99 @@ class App(QWidget):
         layout.addWidget(self.button, 2, 0)
         layout.addWidget(self.label, 3, 0)
         layout.addWidget(self.label2, 4, 0)
-        #rospy.init_node('SpeechCommand')
-        #self.client7 = actionlib.SimpleActionClient('/robot7/move_base',MoveBaseAction)#for now commented out
-        
-        #wait for action client server
-        
+
         self.show()
 
-    def exploration_client(self,robotname,state):
-        rospy.wait_for_service('/'+robotname + '/toggleexploration')
-        try:
-            exploration = rospy.ServiceProxy('/'+robotname + '/toggleexploration', toggleexploration)
-            resp1 = exploration(state)
-        except rospy.ServiceException as e:
-            print("Service call failed: %s"%e)
-
-
-    def moverobot(self, robotno, move):
-        
-        if move == 'left':
-            self.sendGoal(robotno, 0, 1, 1.5708)
-        elif move == 'right':
-            self.sendGoal(robotno, 0, -1, -1.5708)
-        elif move == 'forward':
-            self.sendGoal(robotno, 1, 0, 0)
-        elif move == 'backward':
-            self.sendGoal(robotno, -1, 0, -3.1415)
-
-    def sendGoal(self, robotno, x, y, theta):
-        goal = MoveBaseGoal()
-        goal.target_pose.header.frame_id = "robot" + str(robotno) + "/base_footprint"
-        goal.target_pose.header.stamp = rospy.Time.now()
-        goal.target_pose.pose.position.x = x
-        goal.target_pose.pose.position.y = y
-        # RPY to quaternion convert: 90deg, 0, -90deg
-        orientation = quaternion_from_euler(0, 0, theta)
-        goal.target_pose.pose.orientation.x = orientation[0]
-        goal.target_pose.pose.orientation.y = orientation[1]
-        goal.target_pose.pose.orientation.z = orientation[2]
-        goal.target_pose.pose.orientation.w = orientation[3]
-        
-        print(str(goal))
-        if robotno == 7:
-            try:
-                client7 = actionlib.SimpleActionClient('/robot7/move_base', MoveBaseAction)
-                print("Client started")
-                client7.wait_for_server(rospy.Duration(10))
-            except:
-                print("Could not wait for sever")
-
-            try:
-                client7.send_goal(goal)
-            except:
-                print("Could not send goal")
-        elif robotno == 8:
-            try:
-                client8 = actionlib.SimpleActionClient('/robot8/move_base', MoveBaseAction)
-                print("Client started")
-                client8.wait_for_server(rospy.Duration(10))
-            except:
-                print("Could not wait for sever")
-
-            try:
-                client8.send_goal(goal)
-            except:
-                print("Could not send goal")
-
-        '''wait = client.wait_for_result()
-        if not wait:
-            rospy.logerr("Action server not available!")
-            rospy.signal_shutdown("Action server not available!")
-        else:
-            return client.get_result()'''
+    
     @pyqtSlot()
     def on_click(self):
-        text = ''
-        with sr.Microphone() as source:
-            # read the audio data from the default microphone'
-            audio_data = self.r.listen(source)
-
-            try:
-                text = self.r.recognize_google(audio_data)
-                text = str(text)
-                print(text)
-            except:
-                print("No response from Google")
-        
-        if text != '':
-            # if any words are recognised convert and extract commands
-            text = str.lower(text)
-            token = word_tokenize(text)
-            stems = []
-            for w in token:
-                stems.append(w)
-
-            command = set(self.commands).intersection(stems)
-        
-        else:
-            self.label.setText("No commands recognised. Please try again.")
-            return
-
-        if len(command) != 1:
-            self.label.setText("Command not recognised. Please try again.")
-
-        else:
-            # TODO add navigation message
-            command = list(command)
-            output = "Command not recognised. Please try again."
-            if command[0] == "stop":
-                output = "Command recognised: Stopping"
-            elif command[0] == "move":
+        if self.check_robot7.isChecked() or self.check_robot8.isChecked():
+            text = ''
+            with sr.Microphone() as source:
+                # read the audio data from the default microphone'
+                audio_data = self.r.listen(source)
 
                 try:
-                    move = set(self.movements).intersection(stems)
-                    move = str(list(move)[0])
-                    print(move)
+                    text = self.r.recognize_google(audio_data)
+                    text = str(text)
+                    print(text)
                 except:
-                    move = None
-                    print("No move")
-                if len(command) != 1:
-                    output = "Command not recognised. Please try again."
-                else:
-                    if move != None:
-                        output = "Command recognised: Moving " + move
-                        if self.check_robot7.isChecked() and self.check_robot8.isChecked():
-                            self.label2.setText("Sending command to robot 7 and robot 8")
-                            self.moverobot(7, move)
-                            self.moverobot(8, move)
-                        elif self.check_robot7.isChecked():
-                            self.label2.setText("Sending command to robot 7")
-                            self.moverobot(7, move)
-                        elif self.check_robot8.isChecked():
-                            self.label2.setText("Sending command to robot 8")
-                            self.moverobot(8, move)
-                        else:
-                            self.label2.setText("No robot selected. Command not sent")
-                    else:
-                         output = "Command not recognised: Missing direction"
-                         print("No direction")
-
+                    print("No response from Google")
             
-            elif command[0] == "explore":
-                print("Went to exploration")
+            if text != '':
+                # if any words are recognised convert and extract commands
+                text = str.lower(text)
+                token = word_tokenize(text)
+                stems = []
+                for w in token:
+                    stems.append(w)
 
-                if self.check_robot7.isChecked():
-                    self.label2.setText("Starting exploration on robot 7")
-                    self.exploration_client('robot7', 1)
-                    print("Exploration7 going")
+                command = set(self.commands).intersection(stems)
+            
+            else:
+                self.label.setText("No commands recognised. Please try again.")
+                return
 
-                elif self.check_robot8.isChecked():
-                    self.label2.setText("Starting exploration on robot 8")
-                    self.exploration_client('robot8', 1)
-                    print("Exploration8 going")
-                output = "Command recognised. Exploring"
+            if len(command) != 1:
+                self.label.setText("Command not recognised. Please try again.")
 
-            elif command[0] == "go":
-                #TODO add function to choose posiiton
-                position = "position placeholder"
-                output = "Command recognised. Going to " + position
+            else:
+                # TODO add navigation message
+                command = list(command)
+                output = "Command not recognised. Please try again."
+                if command[0] == "stop":
+                    output = "Command recognised: Stopping"
+                elif command[0] == "move":
+
+                    try:
+                        move = set(self.movements).intersection(stems)
+                        move = str(list(move)[0])
+                        print(move)
+                    except:
+                        move = None
+                        print("No move")
+                    if len(command) != 1:
+                        output = "Command not recognised. Please try again."
+                    else:
+                        if move != None:
+                            output = "Command recognised: Moving " + move
+
+                            if self.check_robot7.isChecked():
+                                self.label2.setText("Sending command to robot 7")
+                                self.robot7.moverobot(move)
+
+                            if self.check_robot8.isChecked():
+                                self.label2.setText("Sending command to robot 8")
+                                self.robot8.moverobot(move)
+
+                        else:
+                            output = "Command not recognised: Missing direction"
+                            print("No direction")
+
+                
+                elif command[0] == "explore":
+                    print("Went to exploration")
+
+                    if self.check_robot7.isChecked():
+                        self.label2.setText("Starting exploration on robot 7")
+                        self.robot7.exploration_client(1)
+                        print("Exploration 7 going")
+
+                    if self.check_robot8.isChecked():
+                        self.label2.setText("Starting exploration on robot 8")
+                        self.robot8.exploration_client(1)
+                        print("Exploration 8 going")
+                    output = "Command recognised. Exploring"
+
+                elif command[0] == "go":
+                    #TODO add function to choose posiiton
+                    position = "position placeholder"
+                    output = "Command recognised. Going to " + position
 
 
-            self.label.setText(output)
+                self.label.setText(output)
+        else:
+            self.label2.setText("No robot selected. Command not sent")
             
 if __name__ == '__main__':
     
---
+++ languageslam/src/languageslam/robotCommands.py
@@ -0,0 +1,61 @@
+import sys
+
+import rospy
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from tf.transformations import quaternion_from_euler
+
+from languageslam.srv import toggleexploration
+
+class robot_command():
+    def __init__(self, robotno):
+        self.robotno = robotno
+        self.robotname = "robot" + str(robotno)
+
+    def exploration_client(self, state):
+        rospy.wait_for_service('/'+ self.robotname + '/toggleexploration')
+        try:
+            exploration = rospy.ServiceProxy('/'+ self.robotname + '/toggleexploration', toggleexploration)
+            resp1 = exploration(state)
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
+
+
+    def moverobot(self, move):
+        
+        if move == 'left':
+            self.sendGoal(0, 1, 1.5708)
+        elif move == 'right':
+            self.sendGoal(0, -1, -1.5708)
+        elif move == 'forward':
+            self.sendGoal(1, 0, 0)
+        elif move == 'backward':
+            self.sendGoal(-1, 0, -3.1415)
+
+    def sendGoal(self, x, y, theta):
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = self.robotname + "/base_footprint"
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = x
+        goal.target_pose.pose.position.y = y
+        # RPY to quaternion convert: 90deg, 0, -90deg
+        orientation = quaternion_from_euler(0, 0, theta)
+        goal.target_pose.pose.orientation.x = orientation[0]
+        goal.target_pose.pose.orientation.y = orientation[1]
+        goal.target_pose.pose.orientation.z = orientation[2]
+        goal.target_pose.pose.orientation.w = orientation[3]
+        
+        print(str(goal))
+
+        try:
+            self.client = actionlib.SimpleActionClient('/' + self.robotname + '/move_base', MoveBaseAction)
+            print("Client started")
+            self.client.wait_for_server(rospy.Duration(10))
+        except:
+            print("Could not wait for sever")
+
+        try:
+            self.client.send_goal(goal)
+        except:
+            print("Could not send goal")
+
